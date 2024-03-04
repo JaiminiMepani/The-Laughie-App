8392e0f78fe0e3118a7a65e49f22e77335659b49
mepanijaimini <20bca024@sanskareducation.org>
2024-03-04 13:15:32 +0000

    final commit

--- app/src/main/java/com/example/thelaughieapp/SignInActivity.kt
+++ app/src/main/java/com/example/thelaughieapp/SignInActivity.kt
@@ -1,5 +1,7 @@
 package com.example.thelaughieapp
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -7,5 +9,11 @@ class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signin)
+
+        val go_to_signup = findViewById<TextView>(R.id.goingonsignuppage)
+        go_to_signup.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/thelaughieapp/SignUpActivity.kt
+++ app/src/main/java/com/example/thelaughieapp/SignUpActivity.kt
@@ -1,5 +1,7 @@
 package com.example.thelaughieapp
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -7,5 +9,11 @@ class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        val go_to_login = findViewById<TextView>(R.id.goingonloginpage)
+        go_to_login.setOnClickListener {
+            val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
